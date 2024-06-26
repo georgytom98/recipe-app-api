f37ffb4a94c16e814fa07dcbeded39ea9d23b30b
georgy <georgytom5463@gmail.com>
2024-06-25 22:54:07 -0500

    API Documentation

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,7 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
 from django.contrib import admin
 from django.urls import path
 
@@ -25,6 +26,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-schema',
-    ),
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
 ]
